d96c84c8775b1af4b1d6f3298156ac86ccd3b1fe
Brad Gessler <bradgessler@gmail.com>
2023-07-20 15:33:33 -0700

    Update README

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Starter
+module Classic
   class Application < Rails::Application
     config.autoload_paths << "#{root}/app/views"
     config.autoload_paths << "#{root}/app/views/layouts"
